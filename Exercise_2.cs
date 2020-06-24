290395b6a7453e0b4451ee5a0963d74555252857
shivkumar98 <shiv_kumar.98@outlook.com>
2020-06-24 11:32:22 +0100

    renamed exercise 1

---
+++ Exercise_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Exercises
+{
+
+    /*Write a program and ask the user to enter their name.
+    Use an array to reverse the name and then store the
+    result in a new string.Display the reversed name on the console.*/
+    class Exercise_2
+    {
+        static public void Main(String[] args)
+        {
+
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        }
+    }
+}
