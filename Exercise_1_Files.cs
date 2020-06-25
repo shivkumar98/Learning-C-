22d1d8129374cf3282ab6ba4715da7c24b8f40f1
shivkumar98 <shiv_kumar.98@outlook.com>
2020-06-25 14:05:34 +0100

    finished exercises for Files

---
+++ Exercise_1_Files.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharp_Exercises
+{
+    class Exercise_1_Files
+    {
+      /*  1- Write a program that reads a text file and displays the number of words.*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Please enter a path:");
+            var path = Console.ReadLine();
+            var text = File.ReadAllText(path);
+            Console.WriteLine( "The text of this file is: ");
+            Console.WriteLine(text);
+
+            var stringText = text.Split(' ');
+            var numberOfWords = stringText.Count();
+
+            Console.WriteLine("The number of words in this text is: "+numberOfWords);
+        }
+    }
+}
---
+++ Exercise_2_Files.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CSharp_Exercises
+{
+    /*2- Write a program that reads a text file and displays the longest 
+    word in the file.*/
+
+    class Exercise_2_Files
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path of a text file: ");
+            var path = Console.ReadLine();
+
+            var text = File.ReadAllText(path);
+            var words = text.Split(' ');
+            List<int> wordLength = new List<int>();
+
+            for (var i =0;i<words.Length;i++)
+            {
+                wordLength.Add(words[i].Length);
+            }
+            var index = wordLength.IndexOf(wordLength.Max());
+            Console.WriteLine("The largest word in this text is: "+ words[index]);
+        }
+
+    }
+
+}
